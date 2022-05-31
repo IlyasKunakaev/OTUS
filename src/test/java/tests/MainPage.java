9fae24a90826950527668e191644fc33a6098e2a
Ilyas Kunakaev <kunakaev-il@ural.rt.ru>
2022-06-01 00:02:30 +0500

    - init commit

---
+++ src/test/java/tests/MainPage.java
@@ -0,0 +1,30 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MainPage {
+    private ChromeDriver driver;
+    private final By title = By.xpath("//div[@class='title-new__text']//h1");
+
+    @BeforeMethod
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void simpleTest() {
+        driver.get("https://otus.ru/");
+    }
+
+    @AfterMethod
+    void tearDown() {
+        driver.close();
+    }
+}
