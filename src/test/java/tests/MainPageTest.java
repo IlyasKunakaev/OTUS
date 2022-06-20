bc1b214f8b07f3c7a2083d22137d823f8d6747d5
Ilyas Kunakaev <kunakaev-il@ural.rt.ru>
2022-06-20 22:16:37 +0500

    - init commit

--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -3,12 +3,11 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class MainPage {
+public class MainPageTest {
     private ChromeDriver driver;
     private final By title = By.xpath("//div[@class='title-new__text']//h1");
 
